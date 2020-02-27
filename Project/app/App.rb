984be4cbc7cc6c492cd14d6191a2831c5c343baf
HSantamaria97 <47119946+HSantamaria97@users.noreply.github.com>
2020-02-26 22:46:18 -0600

    Add files via upload

--- Project/app/App.rb
+++ Project/app/App.rb
@@ -1,13 +1,13 @@
 require "sinatra"
 require 'sinatra/flash'
-require_relative "authentication.rb"
+require_relative "Authentication.rb"
 
 
 #If there is no admin account, make one.
-if User.all(Admin: true).count == 0
+if User.all(admin: true).count == 0
   u = User.new
   u.email = "admin@admin.com"
   u.password = "admin"
-  u.Admin = true
+  u.admin = true
   u.save
 end
