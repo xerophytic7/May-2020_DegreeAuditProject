00c365ace172876aef5983d9267a2d8a26491c30
AlexGiovanni <alexis.santiago01@utrgv.edu>
2020-04-19 20:51:03 -0500

    updated server

--- app.rb
+++ app.rb
@@ -36,6 +36,77 @@ StudentCourses.auto_upgrade!
 AllCourses.auto_upgrade!
 Categories.auto_upgrade!
 
+
+#----------Alexis--------------
+set :public_folder, 'views'
+
+get "/" do
+  
+	erb :index
+end
+
+
+post '/register' do
+	redirect "/dashboard"
+  end
+
+get "/dashboard" do
+  
+	erb :dashboard
+end
+
+post '/home' do
+	redirect "/home"
+  end
+
+get '/home' do
+	erb :home
+  end
+
+post '/advising' do
+	redirect "/advising"
+  end
+
+get '/advising' do
+	erb :advising
+  end
+
+post '/future_courses' do
+	redirect "/future_courses"
+  end
+
+get '/future_courses' do
+	erb :future_courses
+  end
+
+post '/notifications' do
+	redirect "/notifications"
+  end
+
+get '/notifications' do
+	erb :notifications
+  end
+
+post '/user_profile' do
+	redirect "/user_profile"
+  end
+
+get '/user_profile' do
+	erb :user_profile
+  end
+
+get '/classes' do
+	c = File.read("views/js/classes.json")
+	#halt 200, c.to_json
+	#puts JSON.pretty_generate(c)
+	#erb :"songs/index", :locals => { :song_list => songs }
+	#JSON.parse(c)
+	erb :home, :locals =>{c => c.to_json }
+end
+#----------Alexis--------------
+
+
+
 =begin
 #If there is no admin account, make one.
 #************* TEST ******************
