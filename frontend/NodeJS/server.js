7e5f31dc42ce12e98ed08d8433915d9c42d0af81
AlexGiovanni <alexis.santiago01@utrgv.edu>
2020-04-21 23:19:24 -0500

    updated views

--- frontend/NodeJS/server.js
+++ frontend/NodeJS/server.js
@@ -24,9 +24,38 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 //get home page
 app.get('/home', (req, res) => {
-    res.render("home");
+    res.render("home",{active: { home: true },page: "Home"});
 });
 
+//get login page
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+//get register page
+app.get('/register', (req, res) => {
+    res.render("register");
+});
+
+//get advising page
+app.get('/advising', (req, res) => {
+    res.render("advising",{active: { advising: true },page: "Advising"});
+});
+
+//get future_courses page
+app.get('/future_courses', (req, res) => {
+    res.render("future_courses",{active: { future_courses: true },page: "Future Courses"});
+});
+
+//get notifications page
+app.get('/notifications', (req, res) => {
+    res.render("notifications",{active: { notifications: true },page: "Notifications"});
+});
+
+//get user_profile page
+app.get('/user_profile', (req, res) => {
+    res.render("user_profile",{active: { user_profile: true },page: "User Profile"});
+});
 
 // // basic view route
 // app.get('/home', function(req, res, next) {
