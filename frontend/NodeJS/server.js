0b62c527bdba6f7448850b8ecd4888806493f7a7
AlexGiovanni <alexis.santiago01@utrgv.edu>
2020-04-21 22:30:06 -0500

    added handlebars functionality

--- frontend/NodeJS/server.js
+++ frontend/NodeJS/server.js
@@ -1,12 +1,40 @@
-var express = require('express')
-var path = require('path')
 
-app = express()
 
-app.set('port', 3002)
+const express = require('express');
+const path = require('path');
+var hbs = require( 'express-handlebars');
 
-app.use(express.static(path.join(__dirname, 'static')))
+app = express();
+app.set('port', 3002);
+
+// setup handlebars and the view engine for res.render calls
+// (more standard to use an extension like 'hbs' rather than
+//  'html', but the Universiry server doesn't like other extensions)
+app.set('view engine', 'html');
+app.engine( 'html', hbs( {
+  extname: 'html',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+// setup static file service
+app.use(express.static(path.join(__dirname, 'static')));
+
+
+//get home page
+app.get('/home', (req, res) => {
+    res.render("home");
+});
+
+
+// // basic view route
+// app.get('/home', function(req, res, next) {
+//   res.render('home');
+// });
+
+
 
 var server = app.listen(app.get('port'), function() {
 	console.log("Server started...")
-})
+});
