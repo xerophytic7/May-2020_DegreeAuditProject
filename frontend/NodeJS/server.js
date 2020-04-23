f0f4236c072fc52eecd0c66a8cc1fb74e649383a
AlexGiovanni <alexis.santiago01@utrgv.edu>
2020-04-22 22:55:40 -0500

    connected registration to api

--- frontend/NodeJS/server.js
+++ frontend/NodeJS/server.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 const path = require('path');
+//const request = require('request');
+const axios = require('axios').default;
 var hbs = require( 'express-handlebars');
 
 app = express();
 app.set('port', 3002);
 
 // setup handlebars and the view engine for res.render calls
-// (more standard to use an extension like 'hbs' rather than
-//  'html', but the Universiry server doesn't like other extensions)
 app.set('view engine', 'html');
 app.engine( 'html', hbs( {
   extname: 'html',
@@ -18,12 +18,19 @@ app.engine( 'html', hbs( {
   partialsDir: __dirname + '/views/partials/'
 }));
 
+// setup body parsing for form data
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 // setup static file service
 app.use(express.static(path.join(__dirname, 'static')));
 
-
 //get home page
 app.get('/home', (req, res) => {
+
     res.render("home",{active: { home: true },page: "Home"});
 });
 
@@ -56,6 +63,63 @@ app.get('/notifications', (req, res) => {
 app.get('/user_profile', (req, res) => {
     res.render("user_profile",{active: { user_profile: true },page: "User Profile"});
 });
+
+//register a new user with the api
+// http://localhost:4567/api/register
+app.post('/register', (req, res) => {
+    let username = req.body.username.trim();
+    let password = req.body.password.trim();
+	let firstName = req.body.firstName.trim();
+    let lastName = req.body.lastName.trim();
+    //console.log(username, password);
+    //call api with user info
+    axios.post(`http://localhost:4567/api/register?username=${username}&password=${password}&firstName=${firstName}&lastName=${lastName}`
+      
+   ).then(function (response) {
+        console.log(response);
+         //if succsesful
+         res.redirect("/login");
+    })
+    .catch(function (error) {
+        console.log(error);
+         //if succsesful
+        //res.redirect("/login");
+    });
+
+   
+});
+
+
+// //register a new user with the api
+// // http://localhost:4567/api/register
+// app.post('/register', (req, res) => {
+//     let username = req.body.username;
+//     let password = req.body.password;
+// 	let firstName = req.body.firstName;
+//     let lastName = req.body.lastName;
+//     //console.log(username, password);
+//     //call api with user info
+//     axios.post('http://localhost:4567/api/register', {username: username, password: password, 
+//     firstName: firstName,  lastName: lastName, }, 
+//     {headers: {'Accept': 'application/json'}})
+//     .then(function (response) {
+//         console.log(response);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+
+// });
+
+
+
+
+
+
+
+
+
+
 
 // // basic view route
 // app.get('/home', function(req, res, next) {
