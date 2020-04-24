a42e54d60378b2a1a1d254d8758980826140a0a9
mario <mario.velasquez.en@gmail.com>
2020-04-23 19:42:30 -0500

    Figured out how to pull entire lists from JSON

--- frontend/Flutter/lib/studentpages/AddAClass.dart
+++ frontend/Flutter/lib/studentpages/AddAClass.dart
@@ -13,44 +13,33 @@ import 'package:http/http.dart' as http;
 
 final storage = new FlutterSecureStorage();
 
-String firstname = "USER";
-String lastname = "USER";
-String email = "USEREMAIL";
-String  gpa = "0.0";
-String catalogyear = "####-####";
-String classification = "Undergrad";
-String hours = "0";
-String advancedhours = "0";
-String advancedcshours = "0";
-double perA = 0.32;
+final double perA = 0.32;
 
-Future<Void> StudentInfo() async {
-  //String username,password,fn,ln,id;
-  String value = await storage.read(key: "token");
-  print("This is the supposed Token $value");
-  final response = await http.get(
-    "$address/MyInfo",
-    headers: {HttpHeaders.authorizationHeader: "Bearer $value"},
-  );
+// Future<Void> StudentInfo() async {
+//   //String username,password,fn,ln,id;
+//   String value = await storage.read(key: "token");
+//   print("This is the supposed Token $value");
+//   final response = await http.get(
+//     "$address/MyInfo",
+//     headers: {HttpHeaders.authorizationHeader: "Bearer $value"},
+//   );
 
-  Map<String, dynamic> data = json.decode(response.body);
-  print("return the JSON of info ==> $data");
+//   Map<String, dynamic> data = json.decode(response.body);
+//   print("return the JSON of info ==> $data");
 
-  if(data["FirstName"] != "")      firstname       = data["FirstName"];
-  if(data["LastName"] != "")       lastname        = data["LastName"];
-  if(data["Email"] != "")          email           = data["Email"];
-  if(data["GPA"] != "")            gpa             = data["GPA"];
-  if(data["CatalogYear"] != "")    catalogyear     = data["CatalogYear"];   
-  if(data["Classification"] != "") classification  = data["Classification"]; 
-  if(data["Hours"] != "")          hours           = data["Hours"];
-  if(data["AdvancedCSHours"] != "")advancedcshours = data["AdvancedCsHours"];
-  if(data["AdvancedHours"] != "")  advancedhours   = data["AdvancedHours"];
+//   if(data["FirstName"] != "")      firstname       = data["FirstName"];
+//   if(data["LastName"] != "")       lastname        = data["LastName"];
+//   if(data["Email"] != "")          email           = data["Email"];
+//   if(data["GPA"] != "")            gpa             = data["GPA"];
+//   if(data["CatalogYear"] != "")    catalogyear     = data["CatalogYear"];
+//   if(data["Classification"] != "") classification  = data["Classification"];
+//   if(data["Hours"] != "")          hours           = data["Hours"];
+//   if(data["AdvancedCSHours"] != "")advancedcshours = data["AdvancedCsHours"];
+//   if(data["AdvancedHours"] != "")  advancedhours   = data["AdvancedHours"];
 
+//   return null;
 
-
-  return null;
-
-}
+// }
 
 Future<List<dynamic>> CoursesInfo() async {
   //String username,password,fn,ln,id;
@@ -61,11 +50,10 @@ Future<List<dynamic>> CoursesInfo() async {
     headers: {HttpHeaders.authorizationHeader: "Bearer $value"},
   );
 
-  if(response.statusCode != 200) return null;
+  if (response.statusCode != 200) return null;
 
   List<dynamic> data = json.decode(response.body);
   return data;
-
 }
 
 class AddAClass extends StatefulWidget {
@@ -78,21 +66,105 @@ class AddAClass extends StatefulWidget {
 }
 
 class _AddAClassState extends State<AddAClass> {
+
+  Future<List<Course>> _getCourses() async {
+    String value = await storage.read(key: "token");
+
+    var response = await http.get(
+      "$address/all/Courses",
+      headers: {HttpHeaders.authorizationHeader: "Bearer $value"},
+    );
+
+    var data = json.decode(response.body);
+
+    List<Course> courses = [];
+
+    for (var i in data) {
+      Course course = Course(i["CourseID"], i["CourseDept"], i["CourseNum"], i["Name"], i["Institution"]);
+      courses.add(course);
+      
+    }
+    print("This is the number of courses => ${courses.length}");
+    return courses;
+
+  }
+
   int statusCode = 0;
-  
+
   @override
   Widget build(BuildContext context) {
-    StudentInfo();
+    //StudentInfo();
     // Future<Map<String, dynamic>> data = CoursesInfo();
 
     // print(data);
-  //need a future builder
+    //need a future builder
     return new Scaffold(
       backgroundColor: Color(0xff65646a),
-      
-      body: Column(children: <Widget>[
-
-      ],),
+      body: Container(
+        child:
+          FutureBuilder(
+            future: _getCourses(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return new Scaffold(
+                  backgroundColor: Color(0xff65646a),
+                  body: new Center(
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset("assets/images/image0.png"),
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return ListTile(
+                      title: Text(snapshot.data[i].name),                     
+                    );
+                  },
+                );
+              }
+            },
+          )
+      ),
     );
   }
+}
+
+class Student {
+  final String firstname;
+  final String lastname;
+  final String email;
+  final String gpa;
+  final String catalogyear;
+  final String classification;
+  final String hours;
+  final String advancedhours;
+  final String advancedcshours;
+
+  Student(
+      this.firstname,
+      this.lastname,
+      this.email,
+      this.gpa,
+      this.catalogyear,
+      this.classification,
+      this.hours,
+      this.advancedcshours,
+      this.advancedhours);
+}
+
+class Course {
+  final int courseID;
+  final String courseDept;
+  final int courseNum;
+  final String name;
+  final String institution;
+
+  Course(this.courseID, this.courseDept, this.courseNum, this.name,
+      this.institution);
 }
